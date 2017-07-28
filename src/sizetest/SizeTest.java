997e4c03f6edc97887dd778be73bb8a7905eaf6a
magicyang918 <346552561@qq.com>
2017-07-28 19:14:26 +0800

    内存占用空间测试

--- src/sizetest/SizeTest.java
+++ src/sizetest/SizeTest.java
@@ -7,6 +7,9 @@
 
 package sizetest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.zzh.size.MySizeOf;
 
 /**
@@ -15,13 +18,57 @@ import com.zzh.size.MySizeOf;
  */
 public class SizeTest {
 
-	public static void main(String[] args) {
-		System.out.println("new Integer(1) 占用空间为： " + MySizeOf.sizeOf(new Integer(1)));
+	public static void main(String[] args) throws IllegalAccessException {
+		System.out.println("new Integer(1) 自身占用空间为： " + MySizeOf.sizeOf(new Integer(1)));
+		
+		String str = "a";
+		System.out.println("a 自身占用空间为： " + MySizeOf.sizeOf(str));
 
-		System.out.println("new String(a) 占用空间为： " + MySizeOf.sizeOf(new String("a")));
+		System.out.println("new String(a) 自身占用空间为： " + MySizeOf.sizeOf(new String("a")));
 
-		System.out.println("new String(aaaa) 占用空间为： " + MySizeOf.sizeOf(new String("aaaa")));
+		System.out.println("new String(aaaa) 自身占用空间为： " + MySizeOf.sizeOf(new String("aaaa")));
 
+		System.out.println("new String(aaaaaaaaaa) 自身占用空间为： " + MySizeOf.sizeOf(new String("aaaaaaaaaa")));
+
+		System.out.println("new Integer(1) 全部占用空间为： " + MySizeOf.fullSizeOf(new Integer(1)));
+		
+		System.out.println("a 全部占用空间为： " + MySizeOf.fullSizeOf(str));
+
+		System.out.println("new String(a) 全部占用空间为： " + MySizeOf.fullSizeOf(new String("a")));
+
+		System.out.println("new String(aaaa) 全部占用空间为： " + MySizeOf.fullSizeOf(new String("aaaa")));
+
+		System.out.println("new String(aaaaaaaaaa) 全部占用空间为： " + MySizeOf.fullSizeOf(new String("aaaaaaaaaa")));
+
+		A a = new A();
+		System.out.println("new A() 自身占用空间为： " + MySizeOf.sizeOf(a));
+		System.out.println("new A() 全部占用空间为： " + MySizeOf.fullSizeOf(a));
+
+	}
+
+	public static class A {
+//		//1个字节
+//		boolean i1 = false;
+//		//8个字节
+//		long j;
+		String s = "a";
+//		String s1 = new String("a");
+//		List<Integer> list = new ArrayList<>();
+//
+//		B b;
+//
+//		A() {
+//			b = new B();
+//
+//			list.add(1);
+//		}
+
+	}
+
+	public static class B {
+		int i = 0;
+		long j;
+		String s = "a";
 	}
 
 }
